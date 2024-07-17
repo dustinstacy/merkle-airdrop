d5e46f237ec1305bfd1b289deec4d3239dd8d629
Dustin Stacy <dustin.stacy@outlook.com>
2024-07-17 13:32:04 -0500

    fix formatting

--- src/PizzaToken.sol
+++ src/PizzaToken.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-import { ERC20 } from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-import { Ownable } from '@openzeppelin/contracts/access/Ownable.sol';
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title PizzaToken
 /// @author Dustin Stacy
 /// @notice Basic implementation of the ERC20 token for use in Merkle Airdrop Contract.
 contract PizzaToken is ERC20, Ownable {
-    /// @notice The newest version of Open Zeppelin's Ownable contract require's an `initialOwner` to
-    /// be passed to the Ownable constructor.
+    /// @notice The newest version of Open Zeppelin's Ownable contract require's an `initialOwner` to be passed to the
+    /// Ownable constructor.
     /// @notice If using an older version, no `initialOwner` will be required.
     /// @notice This contract sets the `initialOwner` to the deployer.
-    constructor() ERC20('PIZZA', 'PZA') Ownable(msg.sender) {}
+    constructor() ERC20("PIZZA", "PZA") Ownable(msg.sender) { }
 
     /// @param to Address to mint tokens to.
     /// @param amount Amount of tokens to mint.
